a1119b00d430c866d129f3c80e930bf2b408a748
caioMathMarketing <128814343+caioMathMarketing@users.noreply.github.com>
2023-03-24 09:53:50 -0300

    teste

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Server Side</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="dist/index.css">
+
+	<!-- Cole o código do GTM server-side aqui -->
+	<script async src="https://www.googletagmanager.com/GTM-WW5WB8Q.js"></script>
+	<!-- Fim do código do GTM server-side -->
+</head>
+
+<body>
+
+    <header class="container">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-between">
+                <img src="img/logo-santander-red.svg" loading="lazy" alt="logo-santander-red" class="logo-santander-red">
+                <button class="btn" type="submit">Abra já a sua conta</button>
+            </div>
+        </div>
+    </header>
+
+    <div id="banner">
+        <div class="banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-end">
+                        <div class="form">
+                            <h5 class="h1 mb-4">
+                                Em poucos cliques, abra sua conta 100% online.
+                            </h5>
+                            <p class="mb-5">
+                                Sem comprovante de renda e com tudo o que um banco completo oferece.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+    <script src="dist/index.js"></script>
+</body>
+
+</html>
